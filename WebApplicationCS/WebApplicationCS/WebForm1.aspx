970bdf4cf37f1318f11d5754f53954f7074631a1
Diego Cruz Mendes <diegocmendes@msn.com>
2018-11-06 20:19:24 -0200

    aula aspnet

---
+++ WebApplicationCS/WebApplicationCS/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplicationCS.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <h1><strong>Nunca nem vi!!!!</strong></h1>
+        <div>
+            <strong><asp:Label ID="Label1" runat="server" Text="Imagens"></asp:Label></strong>
+        </div>
+        <p>
+            <asp:FileUpload ID="FileUpload1" runat="server" Width="374px" />
+            &nbsp
+            <asp:Button ID="ProcessarButton" runat="server" Text="Processar" Width="145px" OnClick="ProcessarButton_Click" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebApplicationCS/WebApplicationCS/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ImageProcessor;
+using ImageProcessor.Imaging.Filters.Photo;
+
+namespace WebApplicationCS
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ProcessarButton_Click(object sender, EventArgs e)
+        {
+            byte[] inputFile = FileUpload1.FileBytes;
+            MemoryStream imgMemoryStream = new MemoryStream(inputFile);
+            FileStream outputFileStream = new FileStream(Server.MapPath(@"image.jpg"), FileMode.OpenOrCreate);
+
+            ImageFactory image = new ImageFactory();
+            image.Load(imgMemoryStream);
+            image.Filter(MatrixFilters.Invert);
+            image.Save(outputFileStream);
+            outputFileStream.Close();
+
+        }
+    }
+}
